f32db7daf21086b454e853181423f28108a185d8
Shubh Makadia <60586926+IStrikeboomI@users.noreply.github.com>
2024-04-20 22:28:09 -0500

    Added function to get key by name

--- src/hotkey/DeviceKey.h
+++ src/hotkey/DeviceKey.h
@@ -1,11 +1,12 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <string_view>
 struct DeviceKey {
 	//First name is primary name (usaully abbreviated ex. Ctrl)
 	//Names after that are possible names (ex. Control)
-	std::vector<std::string> names;
+	std::vector<std::string_view> names;
 	char value;
 
-	DeviceKey(char value, std::vector<std::string>&& names) : names(names), value(value) {};
+	DeviceKey(char value, std::vector<std::string_view>&& names) : names(names), value(value) {};
 };
--- src/hotkey/DeviceKeys.h
+++ src/hotkey/DeviceKeys.h
@@ -3,6 +3,10 @@
 #include <vector>
 #include <functional>
 #include <Winuser.h>
+#include <cctype>
+#include <algorithm>
+#include <string_view>
+
 #include "DeviceKey.h"
 
 //https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
@@ -16,5 +20,16 @@ namespace DeviceKeys {
 
 	static DeviceKey LBUTTON = add(DeviceKey(VK_LBUTTON,{"LButton","Left Key", "Left Click"}));
 	static DeviceKey RBUTTON = add(DeviceKey(VK_RBUTTON, { "RButton","Right Key", "Right Click" }));
+
+	//Gets device key by case insensitive search 
+	DeviceKey& getByName(const std::string& name) {
+		for (std::reference_wrapper<DeviceKey> key : keys) {
+			for (std::string_view n : key.get().names) {
+				if (std::ranges::equal(std::string_view(name), n, [](auto a, auto b) {return std::tolower(a) == std::tolower(b); })) {
+					return key;
+				}
+			}
+		}
+	}
 
 }
