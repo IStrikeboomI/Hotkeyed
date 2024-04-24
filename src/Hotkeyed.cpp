66764ad7002e53de7d18132aeec996211fc18f2a
Shubh Makadia <60586926+IStrikeboomI@users.noreply.github.com>
2024-04-23 20:07:36 -0500

    Added device mappings

--- src/Hotkeyed.cpp
+++ src/Hotkeyed.cpp
@@ -2,10 +2,10 @@
 #include "windows/Interceptor.h"
 #include "hotkey/HotkeyManager.h"
 #include "hotkey/DeviceKeys.h"
-
+#include <filesystem>
 int main() {
     DeviceManager::populate();
-    std::cout << typeid(int).name() << "\n";
+    DeviceManager::createOrApplyMapping("mapping.mapping");
     //Hotkey hotkey({Key(VK_ADD)}, );
     //HotkeyManager::getInstance().addHotkey(hotkey);
     Interceptor interceptor;
--- src/device/DeviceManager.h
+++ src/device/DeviceManager.h
@@ -7,6 +7,8 @@
 #include <Ntddkbd.h>
 #include <string>
 #include <Hidsdi.h>
+#include <filesystem>
+#include <fstream>
 
 #pragma comment (lib,"Hid.lib")
 namespace DeviceManager {
@@ -43,6 +45,45 @@ namespace DeviceManager {
 			if (r.dwType == RIM_TYPEMOUSE) {
 
 			}
+		}
+	}
+	static bool isNumber(const std::string& s) {
+		return !s.empty() && std::find_if(s.begin(),
+										  s.end(), [](unsigned char c) { return !std::isdigit(c); }) == s.end();
+	}
+	static void applyMapping(const std::string& mappingFile) {
+		if (!mappingFile.ends_with(".mapping")) {
+			throw std::invalid_argument("Not a mapping file!");
+		}
+		std::fstream file(mappingFile);
+		if (file.is_open()) {
+			std::string line;
+			while (std::getline(file, line)) {
+				std::string deviceInterfaceName = line.substr(0, line.find_last_of("="));
+				std::string deviceId = line.substr(line.find_last_of("=") + 1);
+				for (std::shared_ptr<Device> d : devices) {
+					if (d->deviceInterfaceName == deviceInterfaceName) {
+						if (isNumber(deviceId)) {
+							d->id = std::stoi(deviceId);
+						}
+					}
+				}
+			}
+		}
+	}
+	//If mappingFile is empty or doesn't exist, then create a default mapping which is just the devices in order
+	//If file does exist then apply mapping
+	static void createOrApplyMapping(const std::string& mappingFile="") {
+		if (std::filesystem::exists(mappingFile)) {
+			applyMapping(mappingFile);
+		} else {
+			std::fstream file(mappingFile, std::ios::out);
+			if (file.is_open()) {
+				for (std::shared_ptr<Device> d : devices) {
+					file << d->deviceInterfaceName << "=" << d->id << "\n";
+				}
+			}
+			file.close();
 		}
 	}
 }
