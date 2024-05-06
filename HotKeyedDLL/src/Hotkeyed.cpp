b7025026483b8d2206285660cea37780cc8bb45b
Shubh Makadia <60586926+IStrikeboomI@users.noreply.github.com>
2024-05-05 21:58:46 -0500

    Converted Project to lib

--- HotKeyedDLL/src/Hotkeyed.cpp
+++ HotKeyedDLL/src/Hotkeyed.cpp
@@ -3,25 +3,10 @@
 #include "hotkey/HotkeyManager.h"
 #include "keyboard/DeviceKeys.h"
 #include <filesystem>
-#include <process.h>
-BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason,LPVOID lpvReserved) {
-    switch (fdwReason) {
-        case DLL_PROCESS_ATTACH: {
-            DeviceManager::populate();
-            DeviceManager::createOrApplyMapping("mapping.mapping");
-            Interceptor interceptor;
-            interceptor.begin();
-            break;
-        }
-
-        case DLL_THREAD_ATTACH:
-            break;
-
-        case DLL_THREAD_DETACH:
-            break;
-
-        case DLL_PROCESS_DETACH:
-            break;
-    }
-    return TRUE;  // Successful DLL_PROCESS_ATTACH.
+int main() {
+    DeviceManager::populate();
+    DeviceManager::createOrApplyMapping("mapping.mapping");
+    Interceptor interceptor;
+    interceptor.begin();
+    return 0;
 }
