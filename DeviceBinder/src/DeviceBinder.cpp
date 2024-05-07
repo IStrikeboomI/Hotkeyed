1adda26ec1162e2e7fdf3489928bab1d79771850
Shubh Makadia <60586926+IStrikeboomI@users.noreply.github.com>
2024-05-06 21:31:43 -0500

    Added hotkey lib to device binder

--- DeviceBinder/src/DeviceBinder.cpp
+++ DeviceBinder/src/DeviceBinder.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <device/DeviceManager.h>
 
+#pragma comment (lib,"Hotkeyed.lib")
 int main() {
+	DeviceManager::populate();
+	DeviceManager::createOrApplyMapping("mapping.mapping");
+	std::cout << DeviceManager::devices.size() << "\n";
 	return 0;
 }
