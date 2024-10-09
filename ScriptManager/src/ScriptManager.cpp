e65e053b1a574623d38e71f3e15cc51eb96ab08b
IStrikeboomI <shubhmakadia12@gmail.com>
2024-10-09 16:24:41 -0500

    created base for general integer class

--- ScriptManager/src/ScriptManager.cpp
+++ ScriptManager/src/ScriptManager.cpp
@@ -3,17 +3,18 @@
 #include <windows/Interceptor.h>
 #include "script/Script.h"
 #include "engine/ScriptEngine.h"
+#include "engine/Integer.h"
 int main() {
-    DeviceManager::getInstance().populate();
-    Mapping mapping(std::filesystem::current_path().generic_string() + "/mapping.mapping");
-    DeviceManager::getInstance().setMapping(mapping);
-
-    ScriptEngine engine;
-    Script testScript("testScript.htky");
-    engine.addScript(testScript);
+    Integer test(37948);
+    std::cout << test << "\n";
+    //DeviceManager::getInstance().populate();
+    //Mapping mapping(std::filesystem::current_path().generic_string() + "/mapping.mapping");
+    //DeviceManager::getInstance().setMapping(mapping);
+    //
+    //ScriptEngine engine;
+    //Script testScript("testScript.htky");
+    //engine.addScript(testScript);
     //engine.start();
-    //ask for the grades using a while loop and store grade in a double called grade
-    //then do grades.push_back(grade); or whatever the grades vector is called
     return 0;
 
 }
